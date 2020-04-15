6787c794e24ac99a184d67522842c2e7f482f4af
Melody Stern <melliemuse@gmail.com>
2020-04-15 12:22:41 -0500

    set up models, made migrations

---
+++ vendomatic/vendomaticapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VendomaticappConfig(AppConfig):
+    name = 'vendomaticapp'
---
+++ vendomatic/vendomaticapp/models/customer.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .coin import Coin
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    coinId = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_customer(sender, instance, **kwargs):
+    instance.customer.save()
